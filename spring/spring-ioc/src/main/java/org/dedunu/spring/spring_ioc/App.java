f501633d4c3ea40b516c8386693cb4a18ae04d00
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-18 03:31:01 +0530

    Developed sample two classes

---
+++ spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/App.java
@@ -0,0 +1,11 @@
+package org.dedunu.spring.spring_ioc;
+
+/**
+ * Created by dedunu on 11/18/14.
+ */
+public class App {
+    public static void main(String args[]) {
+        Device device = new Device();
+        device.turnOn();
+    }
+}
---
+++ spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/Device.java
@@ -0,0 +1,11 @@
+package org.dedunu.spring.spring_ioc;
+
+/**
+ * Created by dedunu on 11/18/14.
+ */
+public class Device {
+
+    public void turnOn() {
+        System.out.println("Device turned on.");
+    }
+}
