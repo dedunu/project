73c5f94913e613d37e10c0ae1aab8a224f955b17
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-18 03:37:02 +0530

    XmlBeanFactory IOC

--- spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/App.java
+++ spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/App.java
@@ -1,11 +1,16 @@
 package org.dedunu.spring.spring_ioc;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 /**
  * Created by dedunu on 11/18/14.
  */
 public class App {
     public static void main(String args[]) {
-        Device device = new Device();
+        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        Device device = (Device) beanFactory.getBean("device");
         device.turnOn();
     }
 }
