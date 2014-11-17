867c4e0cabd73989ef8925788166c1ed70ab5aee
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-18 04:25:38 +0530

    Added war and jar modules

---
+++ maven/jar-module/src/main/java/org/dedunu/jar_module/Sample.java
@@ -0,0 +1,10 @@
+package org.dedunu.jar_module;
+
+/**
+ * Created by dedunu on 11/18/14.
+ */
+public class Sample {
+    public void doSomethig(){
+        System.out.println("Something did.");
+    }
+}
