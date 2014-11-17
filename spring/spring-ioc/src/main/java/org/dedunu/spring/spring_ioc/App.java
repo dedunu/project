416072ce6124047ed05a50de27f0e293c342629b
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-18 03:44:16 +0530

    Changed BeanFactory to ApplicationContext (Spring)

--- spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/App.java
+++ spring/spring-ioc/src/main/java/org/dedunu/spring/spring_ioc/App.java
@@ -1,16 +1,15 @@
 package org.dedunu.spring.spring_ioc;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by dedunu on 11/18/14.
  */
 public class App {
     public static void main(String args[]) {
-        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Device device = (Device) beanFactory.getBean("device");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Device device = (Device) applicationContext.getBean("device");
         device.turnOn();
     }
 }
