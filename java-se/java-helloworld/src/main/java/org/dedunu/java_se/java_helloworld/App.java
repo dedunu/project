709fa15be48769b9f95faed1916579cbbc5d889e
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-11-18 03:00:58 +0530

    Added java-helloworld submodule

---
+++ java-se/java-helloworld/src/main/java/org/dedunu/java_se/java_helloworld/App.java
@@ -0,0 +1,10 @@
+package org.dedunu.java_se.java_helloworld;
+
+/**
+ * Created by dedunu on 11/18/14.
+ */
+public class App {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
